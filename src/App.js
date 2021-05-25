217dcba673467c516adbb89963ac2b966512b15d
Cendj <lachlan.cendroevans@studytafensw.edu.au>
2021-05-25 10:36:11 +1000

    Fair few changes added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import Logo from './hammer-solid.png';
 import './App.css';
+import {Header} from './components/Header'
 
+const items = [
+  {"name": "Home" , "link" : "/home"},
+  {"name": "The Apartment" , "link" : "/apartment"},
+  {"name": "pricing" , "link" : "/pricing"},
+  {"name": "Book" , "link" : "/Book"},
+  {"name": "Local Area" , "link" : "/local"},
+  {"name": "Contact" , "link" : "/contact"},
+]
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="website">
+     <Header logo = {Logo} nav={items}/> 
+      <main className="content"></main>
+      <footer className="footer"></footer>
+
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+export function Header ( props ) {
+    return (
+        <header className="header">
+            <img className="logo" src={props.logo}></img>
+            <nav classname ="navigation">
+                
+            </nav>
+        </header>
+    )
+}
+export default Header
